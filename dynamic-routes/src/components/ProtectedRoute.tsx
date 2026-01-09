55a9b6b0f3299c270fb9bdac3bcc6c87d885fc5c
unw10181 <uchennanwilliams@gmail.com>
2026-01-09 01:32:55 -0500

    added route context in protection

---
+++ dynamic-routes/src/components/ProtectedRoute.tsx
@@ -0,0 +1,16 @@
+import { Navigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
+
+export default function ProtectedRoute({
+  children,
+}: {
+  children: JSX.Element;
+}) {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+}
